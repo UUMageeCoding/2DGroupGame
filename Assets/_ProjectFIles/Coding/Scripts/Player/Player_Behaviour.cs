9a0a472326b3f3ff765162614480affe97de5565
M-McCorkell156 <McCorkell-M@ulster.ac.uk>
2024-02-27 09:23:55 +0000

    Attemping State control

--- Assets/_ProjectFIles/Coding/Scripts/Player/Player_Behaviour.cs
+++ Assets/_ProjectFIles/Coding/Scripts/Player/Player_Behaviour.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.WSA;
 
-public class Player_Movement : MonoBehaviour
+public class Player_Behaviour : MonoBehaviour
 {
     private float horizontal;
     [SerializeField] private float playerSpeed = 8f;
@@ -31,27 +31,35 @@ public class Player_Movement : MonoBehaviour
     }
     void Update()
     {
-
-
             horizontal = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetButtonDown("Jump") && IsGrounded() && countJump == false)
         {
             Debug.Log("Jump");
+            countJump = true;
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
             //animator.SetBool("IsJump", true);
-            countJump = true;
-
         }
 
         if (Input.GetButtonDown("Jump") && IsGrounded() == false && countJump == true)
         {
-            chute.SetActive(true);
             Debug.Log("Para");
+            chute.SetActive(true);
             rb.drag = 5;
-            playerSpeed = 2;
             countJump = false;
-            Debug.Log(countJump);
+            //Debug.Log(countJump);
+        }
+
+        if (rb.velocity.y < 0f)
+        {
+            Debug.Log("Falling");
+            countJump = true;
+        }
+
+        if (IsGrounded() && countJump == true)
+        {
+            Debug.Log("Reset Jump");
+            countJump = false;
         }
         /*
         if (!Input.GetButton("Jump") && IsGrounded())
@@ -77,18 +85,6 @@ public class Player_Movement : MonoBehaviour
             //Debug.Log("Stop Moving");
             animator.SetBool("IsWalk", false);
             animator.SetBool("IsJump", false);
-        }
-
-        if (rb.velocity.y <= 0f)
-        {
-            animator.SetBool("IsFall", true);
-            animator.SetBool("IsJump", false);
-        }
-
-        if (IsGrounded())
-        {
-            //Debug.Log("on ground");
-            animator.SetBool("IsFall", false);
         }
         */
         Flip();
@@ -116,16 +112,15 @@ public class Player_Movement : MonoBehaviour
         }
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Ground") 
         {
             chute.SetActive(false);
             Debug.Log("Ground");
             rb.drag = 1;
-            playerSpeed = 5;
             countJump = false;
-            Debug.Log(countJump);
+            //Debug.Log(countJump);
         }
     }
 }
