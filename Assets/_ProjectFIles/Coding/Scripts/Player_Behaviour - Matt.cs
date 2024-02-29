9420331a17c7375c28815375383057ef7f894f81
M-McCorkell156 <McCorkell-M@ulster.ac.uk>
2024-02-29 14:18:23 +0000

    reworking

--- Assets/_ProjectFIles/Coding/Scripts/Player_Behaviour - Matt.cs
+++ Assets/_ProjectFIles/Coding/Scripts/Player_Behaviour - Matt.cs
@@ -34,17 +34,22 @@ public class Player_Behaviour : MonoBehaviour
     [SerializeField] private LayerMask groundLayer;
 
     private bool countJump;
-    private bool isJumpPressed = false;
+    private bool chuteUsed;
+
     [SerializeField] private GameObject chute;
 
     public bool canMove;
-
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.drag = 1;
+
         animator = GetComponent<Animator>();
+
         chute.SetActive(false);
+        chuteUsed = false;
+
         ChangeState(playerSpawn);
         LockMove();  
     }
@@ -52,128 +57,68 @@ public class Player_Behaviour : MonoBehaviour
     {
         xAxis = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump"))
-        {
-            isJumpPressed = true;
-        }
         //Press Any Button to Spawn----------------------------------------
         if (currentState == "PlayerSpawn" && Input.anyKey)
         {
             //Debug.Log("Player has spawned");
             UnlockMove();
+            ChangeState(playerIdle);
         }
         //-----------------------------------------------------------------
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(xAxis * playerSpeed, rb.velocity.y);
-
         if (canMove)
         {
-            //Moving-------------------------------------------------------           
+            rb.velocity = new Vector2(xAxis * playerSpeed, rb.velocity.y);
 
-            if (IsGrounded() && !isJumpPressed)
+            if (Input.GetButtonDown("Jump"))
             {
-                rb.drag = 1;
-                chute.SetActive(false);
-
-                if (xAxis != 0)
+                if (IsGrounded())
                 {
-                    ChangeState(playerIdle);
+                    Debug.Log("Jump");
+                    ChangeState(playerJump);
+                    rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
                 }
-                else
-                {
-                    ChangeState(playerRun);
-                }
-            }                    
-            //-------------------------------------------------------------
-
-            //Jumping------------------------------------------------------
-            if (isJumpPressed && IsGrounded())
-            {
-                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-                isJumpPressed = false;
-                ChangeState(playerJump);
-                //Debug.Log("Player has Jumped");
             }
-            //Chute--------------------------------------------------------
             /*
-            if (!isJumpPressed && !IsGrounded())
+            if (Input.GetButtonDown("Jump") && IsGrounded() == false)
             {
+                Debug.Log("Chute");
+                ChangeState(playerChute);
                 chute.SetActive(true);
                 rb.drag = 5;
-                //Debug.Log("Player has Chuted");
+                chuteUsed = true;
+                //Debug.Log(countJump);
             }
             */
+            if (rb.velocity.y < 0f)
+            {
+                //Debug.Log("Falling");
+                countJump = true;
+            }
 
-            //-------------------------------------------------------------
+            if (IsGrounded() && chuteUsed == true)
+            {
+                //Debug.Log("Reset Jump");
+                countJump = false;
+            }
 
+            if (Input.GetButton("Horizontal"))
+            {
+                //Debug.Log("Moving");
+                ChangeState(playerRun);
+            }
+
+            
+            Flip();
         }
-
-        /*
-
-
-        if (Input.GetButtonDown("Jump") && IsGrounded() && countJump == false)
-        {
-            Debug.Log("Jump");
-            countJump = true;
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-            //animator.SetBool("IsJump", true);
-        }
-
-        if (Input.GetButtonDown("Jump") && IsGrounded() == false && countJump == true)
-        {
-            Debug.Log("Para");
-            chute.SetActive(true);
-            rb.drag = 5;
-            countJump = false;
-            //Debug.Log(countJump);
-        }
-
-        if (rb.velocity.y < 0f)
-        {
-            Debug.Log("Falling");
-            countJump = true;
-        }
-
-        if (IsGrounded() && countJump == true) 
-        {
-            Debug.Log("Reset Jump");
-            countJump = false;
-        }
-
-        if (!Input.GetButton("Jump") && IsGrounded())
-        {
-            //Debug.Log("Landed");
-            animator.SetBool("IsJump", false);
-        }
-
-        if (Input.GetButton("Horizontal") && IsGrounded())
-        {
-            //Debug.Log("Moving");
-            animator.SetBool("IsWalk", true);
-        }
-
-        if (Input.GetButtonDown("Horizontal") && !IsGrounded())
-        {
-            //Debug.Log("Flying");
-            animator.SetBool("IsWalk", false);
-        }
-
-        if (Input.GetButtonUp("Horizontal") && IsGrounded())
-        {
-            //Debug.Log("Stop Moving");
-            animator.SetBool("IsWalk", false);
-            animator.SetBool("IsJump", false);
-        }
-        */
-        Flip();
-        
     }
 
     private bool IsGrounded()
     {
+        Debug.Log("Ground");
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
@@ -205,7 +150,7 @@ public class Player_Behaviour : MonoBehaviour
     {
         if (currentState == newState) return;
         {
-            Debug.Log("Current State - " + currentState);
+            //Debug.Log("Current State - " + currentState);
             //animator.Play(newState);
             currentState = newState;
         }
@@ -214,9 +159,11 @@ public class Player_Behaviour : MonoBehaviour
     public void LockMove ()
     {
         canMove = false;
+        //Debug.Log("Lock Move");
     }
     public void UnlockMove()
     {
         canMove = true;
+        //Debug.Log("Unlock Move");
     }
 }
