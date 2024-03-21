730dd20ff1a358b2be74f61bc8a9ee6dda2f9b0d
M-McCorkell156 <McCorkell-M@ulster.ac.uk>
2024-03-21 10:38:53 +0000

    Working on fade In UI

--- Assets/_ProjectFIles/Coding/Scripts/World/Title_Manager_Matt.cs
+++ Assets/_ProjectFIles/Coding/Scripts/World/Title_Manager_Matt.cs
@@ -7,14 +7,20 @@ public class World_Manager_Matt : MonoBehaviour
     #region Variables
 
     private Canvas canvas;
+    public CanvasGroup canvasGroup;
 
     private bool activeStart;
+
+    private int alphaValue;
 
     #endregion
     void Start()
     {
-        Debug.Log("Start Timer");
-        StartCoroutine(LoadTime(3,1));
+        canvas = GetComponent<Canvas>();
+        canvas.enabled = false;
+        //Debug.Log("Start Timer");
+        canvasGroup.alpha = 0f;
+        StartCoroutine(LoadTime(3f, 1));
     }
 
     void Update()
@@ -22,26 +28,37 @@ public class World_Manager_Matt : MonoBehaviour
         if(Input.anyKey && activeStart == true)
         {
             //Debug.Log("Away Start");
+            FadeOut();
             activeStart = false;
-        }
-    }
-    private void FixedUpdate()
-    {
-        if(activeStart == false)
-        {
             canvas.enabled = false;
         }
     }
     private void ActivateScreen()
     {
+
         activeStart = true;
-        canvas = GetComponent<Canvas>();
         canvas.enabled = true;
+        while (alphaValue != 1)
+        {
+            StartCoroutine(LoadTime(0.2f, 0));
+            canvasGroup.alpha += 0.1f;
+            canvasGroup.alpha = alphaValue;
+        }
+
     }
-    private IEnumerator LoadTime (int totalTime, int idTime)
+    private void FadeIn()
+    {
+        
+    }
+    private void FadeOut()
+    { 
+        
+    }
+
+    private IEnumerator LoadTime (float totalTime, int idTime)
     {
         yield return new WaitForSeconds(totalTime);
-        Debug.Log("End Timer");
+        //Debug.Log("End Timer");
 
         if(idTime == 1)
         {
